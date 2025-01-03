b1b43f420a7597b248d0414885f003b003f1ac43
Pawan Kumar <104243843+pawan0505@users.noreply.github.com>
2025-01-03 19:30:46 +0530

    init

---
+++ auth.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = '';
+    for (let i=0; i<32; i++) {
+        //user a simple function here
+        token += options[Math.floor(Math.random()* options.length)];
+    }
+    return token;
+}
+
+app.post("/signup", (req, res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username,
+        password
+    })
+    res.send({
+        message: "You have signed up"
+    })
+    console.log(users);
+});
+
+app.post("/signin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+
+    for (let i = 0; i<users.length; i++){
+        if (users[i].username == username && users[i].password == password) {
+            foundUser = users[i]
+        }
+    }
+    if (foundUser) {
+        const token = generateToken();
+        foundUser.token = token;
+        res.json({
+            message: "signin successful",
+            token: token
+        })
+    } else {
+        res.status(403).send({
+            message: "Invalid username or password"
+        })
+    }
+    console.log(users);
+})
+
+app.get("/me", (req, res) => {
+    const token = req.header.token
+    let user = users.find(user => users.token === token);
+    if (user) {
+        res.send({
+            username: user.username,
+            password: user.password
+        })
+    } else {
+        res.status(401).send({
+            message: "token invalid"
+        })
+    }
+})
+
+app.listen(3000, () => {
+    console.log("server is running on http://localhost:3000");
+})
